6f0d7e716eed141e85c567650bc1f52531468171
wallaceyuan <1272864289@qq.com>
2016-05-06 13:36:02 +0800

    123

--- task/config.js
+++ task/config.js
@@ -1,10 +1,10 @@
 
 
-/*var client = require("redis").createClient(6379, "knews-redis2.nrm01e.ng.0001.cnn1.cache.amazonaws.com.cn");
-var ip = 'http://54.222.215.248:80';*/
+var client = require("redis").createClient(6379, "knews-redis2.nrm01e.ng.0001.cnn1.cache.amazonaws.com.cn");
+var ip = 'http://54.222.215.248:80';
 
-var client   = require("redis").createClient();
-var ip = 'http://127.0.0.1:3000';
+/*var client   = require("redis").createClient();
+var ip = 'http://127.0.0.1:3000';*/
 
 var mysql = require('mysql');
 var pool = mysql.createPool({
--- task/user.js
+++ task/user.js
@@ -133,7 +133,10 @@ exports.messageValidate   = function(data,callback){
         headers: {'Content-Type': 'application/x-www-form-urlencoded'},
         url: 'http://kankanews.cn-north-1.eb.amazonaws.com.cn/KKShielder',
         method: 'POST',
-        body:"words="+res.message+res.nickName
+/*
+        body:"words="+res.message+res.nickName,
+*/
+        body:"words="+res.message
     };
     request(codeOpt,function(err,result,body){
         var body = JSON.parse(body);
