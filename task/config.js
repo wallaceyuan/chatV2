20687629ac43e87a6345ac6a6be374d4af5ee132
wallaceyuan <1272864289@qq.com>
2016-05-17 09:31:48 +0800

    ip host

--- task/config.js
+++ task/config.js
@@ -1,9 +1,8 @@
 
 var mysql = require('mysql');
 
-/*
-var client = require("redis").createClient(6379, "knews-redis2.nrm01e.ng.0001.cnn1.cache.amazonaws.com.cn");
-var ip = 'http://54.222.215.248:80';
+/*var client = require("redis").createClient(6379, "knews-redis2.nrm01e.ng.0001.cnn1.cache.amazonaws.com.cn");
+var ip = 'http://danmaku.kankanews.com';
 var host = 'knews-redis2.nrm01e.ng.0001.cnn1.cache.amazonaws.com.cn';
 var pool = mysql.createPool({
     host:'kankanewsapi.cjspd4t43dgd.rds.cn-north-1.amazonaws.com.cn',
@@ -11,8 +10,7 @@ var pool = mysql.createPool({
     password:'kankanewsaws2016',
     database:'kk_danmaku',
     connectTimeout:30000
-});
-*/
+});*/
 
 var client   = require("redis").createClient();
 var ip = 'http://127.0.0.1:3000';
